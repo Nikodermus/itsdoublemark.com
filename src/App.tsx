0c2a431c2c2a835a73cb36ddb4a98c11402cd578
Nicolas M. Pardo <nicolasm@nikodermus.media>
2025-05-06 13:24:39 -0500

    refactor: simplify App component and enhance styling with animated text

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const text = "¡double mark!"
 
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="card">
+      <h1>
+        {text.split("").map((letter, index) => (
+          <span key={index} className="letter" style={{animationDelay: `${index * 0.1}s`}}>
+            {letter}
+          </span>
+        ))}
+      </h1>
+    </div>
   )
 }
 
